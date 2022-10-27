7dfb6f8f042f1b4772975873623144e0dbe543b9
yuhsuanlii <101781321+yuhsuanlii@users.noreply.github.com>
2022-10-27 13:51:08 +0800

    Update app.py

--- week-6/app.py
+++ week-6/app.py
@@ -23,14 +23,16 @@ def signup():
         if name == "" or username == "" or password == "": 
             return redirect(url_for('error', message='請填寫所有欄位'))
         # 搜尋資料庫，阻擋已存在帳號
-        sql = "SELECT * FROM member WHERE username = '"+ username + "'"
-        cur.execute(sql)
+        sql = "SELECT * FROM member WHERE username = %s"
+        params = username
+        cur.execute(sql, params)
         data = cur.fetchone()        
         if not data == None:
             session.pop('username', None)
             return redirect(url_for('error', message='帳號已經被註冊'))
-        sql_insert = "INSERT INTO member(name,username,password)VALUES('"+name+"','"+username+"','"+password+"')" 
-        cur.execute(sql_insert)
+        sql_insert = "INSERT INTO member(name,username,password)VALUES(%s, %s, %s)" 
+        params_insert = (name, username, password)
+        cur.execute(sql_insert, params_insert)
         conn.commit() 
         # 存入資料庫後清除session回到首頁重新登入
         session.pop('username', None)
@@ -44,28 +46,29 @@ def signin():
     session['username'] = request.form['username'] 
 
     if request.method == 'POST':
-            # 阻擋空白帳密
-            if username == "" and password == "":
-                # 清除錯誤session避免直接更改URL
-                session.pop('username', None)
-                return redirect(url_for('error', message='請輸入帳號、密碼'))
-            # 搜尋資料庫，判斷帳號是否存在
-            sql = "SELECT name,username,password FROM member WHERE username = '"+ username + "'"
-            cur.execute(sql)
-            data = cur.fetchone()
-            if data == None:
-                # 清除錯誤session避免直接更改URL
-                session.pop('username', None)
-                return redirect(url_for('error', message='帳號、或密碼輸入錯誤'))
-            # 判斷對應密碼是否正確
-            mypwd=data[2] # 輸入的帳號對應的密碼位置 SELECT X,X,password....
-            # print(mypwd)
-            if (mypwd != password):
-                # 清除錯誤session避免直接更改URL
-                session.pop('username', None)
-                return redirect(url_for('error', message='密碼輸入錯誤'))
-            else:
-                return redirect(url_for("member"))                       
+        # 阻擋空白帳密
+        if username == "" and password == "":
+            # 清除錯誤session避免直接更改URL
+            session.pop('username', None)
+            return redirect(url_for('error', message='請輸入帳號、密碼'))
+        # 搜尋資料庫，判斷帳號是否存在
+        sql = "SELECT name,username,password FROM member WHERE username = %s"
+        params = username
+        cur.execute(sql, params)
+        data = cur.fetchone()
+        if data == None:
+            # 清除錯誤session避免直接更改URL
+            session.pop('username', None)
+            return redirect(url_for('error', message='帳號、或密碼輸入錯誤'))
+        # 判斷對應密碼是否正確
+        mypwd=data[2] # 輸入的帳號對應的密碼位置 SELECT X,X,password....
+        # print(mypwd)
+        if (mypwd != password):
+            # 清除錯誤session避免直接更改URL
+            session.pop('username', None)
+            return redirect(url_for('error', message='密碼輸入錯誤'))
+        else:
+            return redirect(url_for("member"))                       
     else: 
         return render_template("index.html")
 
@@ -75,8 +78,9 @@ def member():
     if "username" in session:
         print(f'Signed in as {session["username"]}')
         username = session["username"]
-        sql = "SELECT name,username FROM member WHERE username = '"+ username + "'"
-        cur.execute(sql)
+        sql = "SELECT name,username FROM member WHERE username = %s"
+        params = username
+        cur.execute(sql, params)
         data = cur.fetchone()
         myname = data[0]
         # print(myname)
